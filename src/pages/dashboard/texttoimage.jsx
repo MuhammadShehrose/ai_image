0715e09ebdda5361f0d0c5e46c2b13b71dc18a6f
asharibhussain <asharibhussainofficial@gmail.com>
2025-05-24 19:18:19 +0500

    solve the error img gengerator

--- src/pages/dashboard/texttoimage.jsx
+++ src/pages/dashboard/texttoimage.jsx
@@ -311,7 +311,7 @@ export default function TextToImage() {
       url: "https://ai-text-to-image-generator-flux-free-api.p.rapidapi.com/aaaaaaaaaaaaaaaaaiimagegenerator/fluximagegenerate/generateimage.php",
       headers: {
         "x-rapidapi-key": apiKeyData.api_key,
-        "x-rapidapi-host": "http://ai-text-to-image-generator-flux-free-api.p.rapidapi.com",
+        "x-rapidapi-host": "ai-text-to-image-generator-flux-free-api.p.rapidapi.com",
         "Content-Type": "application/x-www-form-urlencoded",
       },
       data: encodedParams,
